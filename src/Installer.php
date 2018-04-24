2302b5757db5d10e91f07463e4cb22c68b6fa492
Dmytro Karpovych <ZAYEC77@gmail.com>
2018-04-25 01:20:33 +0300

    add Installer

---
+++ src/Installer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace nullref\eav;
+
+
+use nullref\core\components\ModuleInstaller;
+
+/**
+ * Class Installer
+ *
+ * @package nullref\eav
+ */
+class Installer extends ModuleInstaller
+{
+    public function getModuleId()
+    {
+        return 'eav';
+    }
+} 
