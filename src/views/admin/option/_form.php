aba3df4791b9b538fb2bec83d9868a83af083094
Dmytro Karpovych <ZAYEC77@gmail.com>
2018-05-31 14:17:34 +0300

    fix option value input

--- src/views/admin/option/_form.php
+++ src/views/admin/option/_form.php
@@ -17,7 +17,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'sort_order')->textInput() ?>
 
-    <?= $form->field($model, 'value')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'value')->textarea() ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('eav', 'Create') : Yii::t('eav', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
