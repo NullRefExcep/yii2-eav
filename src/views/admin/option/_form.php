b1fc6ae207db2c990f667dbe39ecaca83f5ddfca
Dmytro Karpovych <ZAYEC77@gmail.com>
2019-11-23 23:25:28 +0200

    Update _form.php

--- src/views/admin/option/_form.php
+++ src/views/admin/option/_form.php
@@ -13,7 +13,8 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'attribute_id')->dropDownList(Attribute::find()->withOptions()->getMap()) ?>
+    <?= $form->field($model, 'attribute_id')->dropDownList(Attribute::find()
+        ->andWhere(['type' => Attribute::TYPE_OPTION])->getMap()) ?>
 
     <?= $form->field($model, 'sort_order')->textInput() ?>
 
