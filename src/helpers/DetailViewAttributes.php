852437b8a1bd90f7111be78511e366b5211befa5
Dmytro Karpovych <ZAYEC77@gmail.com>
2020-04-11 03:21:08 +0300

    add DetailViewAttributes helper

---
+++ src/helpers/DetailViewAttributes.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author    Dmytro Karpovych
+ * @copyright 2020 NRE
+ */
+
+
+namespace nullref\eav\helpers;
+
+
+use nullref\eav\models\Entity;
+
+class DetailViewAttributes
+{
+    /**
+     * @param Entity $eavModel
+     * @return array
+     */
+    public static function get(Entity $eavModel)
+    {
+        $attributes = [];
+        foreach ($eavModel->getAttributesConfig() as $attr => $config) {
+            $value = $eavModel->{$attr};
+            if (is_array($value)) {
+                $items = isset($config['items']) ? array_map(function ($value) use ($config) {
+                    return $config['items'][$value];
+                }, $value) : $value;
+                $value = implode(', ', $items);
+            } else {
+                $value = isset($config['items']) ? ($config['items'][$value] ?? '') : $value;
+            }
+            $attributes[] = [
+                'label' => $config['name'],
+                'value' => $value,
+            ];
+        }
+        return $attributes;
+    }
+}
